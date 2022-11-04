5f45f37bdd7395941cc10c151d1e519295d85216
BSDnullset <null_regex@tuta.io>
2022-11-04 12:49:01 -0400

    new changes

--- greet_test.go
+++ greet_test.go
@@ -2,9 +2,9 @@ package main
 
 import "testing"
 
-func test_greet(t *testing.T) {
+func TestGreet(t *testing.T) {
 	got := greet()
-	want := "gsb"
+	want := "clusters slice segregation"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
