7e22bdaa3cd4a973201aea2d1bf6e365c2315d4e
BSDnullset <null_regex@tuta.io>
2022-11-04 10:00:59 -0400

    new changes

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+)
+
+func addKubernetes_Clusters(clusters []string, cluster string) []string (
+	return append(clusters, cluster)
+)
+
 func main() {
-	fmt.println("first commit on github")
+	fmt.println("clusters slice segregation")
+	clusters := []string{} // this is a void cluster slice
+	result := addKubernetes_Clusters(clusters, "alpine")
+	result := addKubernetes_Clusters(clusters, "fedora")
+	resutl := addKubernetes_Clusters(clusters, "ubuntu")
+	result := addKubernetes_Clusters(clusters, "debian")
+	fmt.println(result)
+	// will print the clusters: (alpine, fedora, ubuntu, debian)
 }
