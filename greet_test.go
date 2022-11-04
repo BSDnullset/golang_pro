5d247d0bbb6a506859f06e805c3da69e7b6b0207
BSDnullset <null_regex@tuta.io>
2022-11-04 12:53:22 -0400

    new commit

--- greet_test.go
+++ greet_test.go
@@ -2,6 +2,10 @@ package main
 
 import "testing"
 
+// remember to run:$ go mod init SOMENAME
+// in each new folder before running commands like: $ go test
+// or: $ go build
+
 func TestGreet(t *testing.T) {
 	got := greet()
 	want := "clusters slice segregation"
