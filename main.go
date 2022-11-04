fc7b9b1d9aead7b2d3512feb3102f3b6eefcbf14
BSDnullset <null_regex@tuta.io>
2022-11-04 16:22:04 -0400

    new changes

--- main.go
+++ main.go
@@ -6,9 +6,13 @@ func addKubernetes_Clusters(clusters []string, cluster string) []string {
 	return append(clusters, cluster)
 }
 
-func greet() string {
-	return "clusters slice segregation"
+func greet() string { return "clusters slice segregation" }
+
+func addClusterID(clusters []int, cluster int) []int {
+	return append(clusters, cluster)
 }
+
+func displayID_number() string { return "clusters have have unique identifiers" }
 
 func main() {
 	fmt.Println(greet())
@@ -19,4 +23,12 @@ func main() {
 	result = addKubernetes_Clusters(result, "debian")
 	fmt.Println(result)
 	// will print the clusters: (alpine, fedora, ubuntu, debian)
+
+	fmt.Println(displayID_number())
+	clusterID := []int{}
+	ID_ofCluster := addClusterID(clusterID, 293)
+	ID_ofCluster = addClusterID(ID_ofCluster, 734)
+	ID_ofCluster = addClusterID(ID_ofCluster, 835)
+	ID_ofCluster = addClusterID(ID_ofCluster, 452)
+	fmt.Println(ID_ofCluster)
 }
